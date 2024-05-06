7a9e6b3647e39b69a3d30d4ac2d39b04b56b46eb
Ellen Mateus <ellenrms@gmail.com>
2024-05-06 17:20:16 -0300

    fim do dia

--- src/main/java/com/ellenmateus/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/ellenmateus/workshopmongo/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.ellenmateus.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,8 @@ import com.ellenmateus.workshopmongo.domain.User;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String>{
+
+	void save(List<User> asList);
 
 	
 	
