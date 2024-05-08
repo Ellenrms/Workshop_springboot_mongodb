c4c2dfc6f4e623049aa4c237c131b23b6553eea8
Ellen Mateus <ellenrms@gmail.com>
2024-05-08 16:15:21 -0300

    Consulta simples com query methods

--- src/main/java/com/ellenmateus/workshopmongo/dto/AuthorDTO.java
+++ src/main/java/com/ellenmateus/workshopmongo/dto/AuthorDTO.java
@@ -1,5 +1,36 @@
-package com.ellenmateus.workshopmongo.dto;
+package com.nelioalves.workshopmongo.dto;
 
-public class AuthorDTO {
+import java.io.Serializable;
 
+import com.ellenmateus.workshopmongo.domain.User;
+
+public class AuthorDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private String id;
+	private String name;
+	
+	public AuthorDTO() {
+	}
+	
+	public AuthorDTO(User obj) {
+		id = obj.getId();
+		name = obj.getName();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
